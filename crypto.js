1d313c87d3452b943b2f27956388523b7833beb3
Md Waqar Tabish <waqs2807@gmail.com>
2022-06-23 02:48:16 +0530

    updated hash generation

--- crypto.js
+++ crypto.js
@@ -1,7 +1,12 @@
-const crypto = require('crypto');
+// For triggering generate button to create a hash 
 
-const secret = 'abcdefg';
-const hash = crypto.createHmac('sha256', secret)
-                   .update('I love cupcakes')
-                   .digest('hex');
-console.log(hash);
+function generateHash() {
+    const text = $("#file-name").text();
+    if (text.length !== 0) {
+        const hashText = CryptoJS.SHA1(text);
+        document.getElementById("hashText").innerHTML = hashText;
+    }
+    else{
+        alert("Please Upload The File.")
+    }
+}
